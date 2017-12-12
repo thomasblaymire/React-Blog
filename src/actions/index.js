fc7e95d215633ef57513cac6c555f06f60b6f12a
Tom Blaymire <tblaymire1994@gmail.com>
2017-12-12 22:35:32 +0000

    Render a list of blog posts

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-export costs FETCH_POSTS = 'fetch_posts';
+export const FETCH_POSTS = 'fetch_posts';
 
-const ROOT_URL = 'http://reduxblog.herokuapp.com/api/posts';
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=PAPERCLIP1234';
 
 export function fetchPosts() {
