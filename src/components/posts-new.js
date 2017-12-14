631d2743a54533bacc2b07ca0e61c9d41a6af921
Thomas Blaymire <thomas.blaymire@hermes-europe.co.uk>
2017-12-14 10:06:33 +0000

    Added full form validation with bootstrap destructoring

--- src/components/posts-new.js
+++ src/components/posts-new.js
@@ -4,22 +4,35 @@ import { Field, reduxForm } from 'redux-form';
 class PostsNew extends Component {
 
   renderField(field) {
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+
     return (
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input
           className="form-control"
           type="text"
           {...field.input}
         />
-      </div>
+        <div className="text-help">
+            {touched ? error : ''}
+        </div>
+       </div>
     );
   }
 
+  onSubmit(values) {
+      console.log(values);
+  }
+
   render() {
+    const { handleSubmit } = this.props;
+
+
     return (
       <div>
-        <form>
+        <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
           <Field
             label="Title"
             name="title"
@@ -35,6 +48,7 @@ class PostsNew extends Component {
             name="content"
             component={this.renderField}
           />
+          <button type="submit" className="btn btn-primary">Submit</button>
         </form>
       </div>
     );
